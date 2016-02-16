fa02fbe8b72c85bfdbe424a4c76ba4a1775ca974
Zeeshan Abid <zeeshan595@googlemail.com>
2016-02-16 05:40:37 +0000

    Bug Fixes

--- NeuralNetwork/RNN.cs
+++ NeuralNetwork/RNN.cs
@@ -8,21 +8,32 @@ namespace NeuralNetwork
     {
         public Network[] networks;
 
-        public RNN(int input, int hidden, int output, bool connectAllNodes)
+        public RNN(int input, int hidden, int output, int amount, bool connectAllNodes)
         {
-            networks = new Network[4];
+            if (amount < 2)
+                throw new Exception("Amount needs to be greater then 1");
+
+            networks = new Network[amount + 1];
             networks[0] = new Network(new int[] { input, hidden }, connectAllNodes);
-            networks[1] = new Network(new int[] { input + hidden, hidden }, connectAllNodes);
-            networks[2] = new Network(new int[] { input + hidden, hidden }, connectAllNodes);
-            networks[3] = new Network(new int[] { hidden, output }, connectAllNodes);
+            for (int i = 1; i < networks.Length - 1; i++)
+            {
+                networks[i] = new Network(new int[] { input + hidden, hidden }, connectAllNodes);
+            }            
+            networks[networks.Length - 1] = new Network(new int[] { hidden, output }, connectAllNodes);
         }
 
-        public float[] Compute(float[] xValues1, float[] xValues2, float[] xValues3)
+        public float[] Compute(float[][] xValues)
         {
-            float[] outputs1 = networks[0].Compute(xValues1);
-            float[] outputs2 = networks[1].Compute(CombineLayers(xValues2, outputs1));
-            float[] outputs3 = networks[2].Compute(CombineLayers(xValues3, outputs2));
-            return networks[3].Compute(outputs3);
+            if (xValues.Length != networks.Length - 1)
+                throw new Exception("input's length length doesn't match RNN");
+
+            float[][] outputs = new float[xValues.Length][];
+            outputs[0] = networks[0].Compute(xValues[0]);
+
+            for (int i = 1; i < xValues.Length - 1; i++)
+                outputs[i] = networks[1].Compute(CombineLayers(xValues[i], outputs[i - 1]));
+
+            return networks[3].Compute(outputs[outputs.Length - 1]);
         }
 
         private float[] CombineLayers(float[] input, float[] hidden)
